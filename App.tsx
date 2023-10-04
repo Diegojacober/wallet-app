81e341b9d127813b700d95cbdd0e6dc427430be4
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-04 16:46:17 -0300

    Configurando status bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { View, Text, StatusBar } from "react-native";
-import * as Font from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import { ThemeProvider } from "styled-components";
 import * as SplashScreen from "expo-splash-screen";
 
 import {
@@ -15,22 +16,24 @@ import {
 import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
+import COLORS from "./src/styles/theme";
+
 const App: React.FC = () => {
     const [appReady, setAppReady] = useState(false);
+    const [fontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular,
+    });
 
     useEffect(() => {
         (async () => {
             try {
                 await SplashScreen.preventAutoHideAsync();
-                await Font.loadAsync({
-                    Poppins_300Light: Poppins_300Light,
-                    Poppins_400Regular: Poppins_400Regular,
-                    Poppins_500Medium: Poppins_500Medium,
-                    Poppins_700Bold: Poppins_700Bold,
-                    Poppins_800ExtraBold: Poppins_800ExtraBold,
-                    DMSans_400Regular: DMSans_400Regular,
-                    DMSerifDisplay_400Regular: DMSerifDisplay_400Regular,
-                });
             } catch (e) {
                 console.warn(e);
             } finally {
@@ -50,9 +53,18 @@ const App: React.FC = () => {
     }
 
     return (
-        <View onLayout={onLayout}>
-            <StatusBar backgroundColor={"#e97cf1"} />
-        </View>
+        <ThemeProvider theme={COLORS}>
+            <StatusBar style="dark" translucent backgroundColor="transparent" />
+            <View
+                onLayout={onLayout}
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: COLORS.COLORS.ATTENTION_LIGHT9,
+                }}
+            ></View>
+        </ThemeProvider>
     );
 };
 
